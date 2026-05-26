8b41889bd430a56391617ae5bd27e4c18932408a
htfci <133967843+htfci@users.noreply.github.com>
2026-05-26 09:59:35 +0200

    Add router/

--- app/agent-eval-service/src/main.py
+++ app/agent-eval-service/src/main.py
@@ -1,11 +1,12 @@
-import uvicorn
 from fastapi import FastAPI
+from router.run import router as run_router
 import uvicorn
 
 
 app = FastAPI()
 
-# app.include_router()
+app.include_router(run_router)
+
 
 def main():
     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="debug", reload=True)
